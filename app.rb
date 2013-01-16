5051162b4db97ac2e99540e869bda2243ec44a46
endenis <endenis@gmail.com>
2013-01-17 00:23:52 +0400

    Implemented words counting

--- app.rb
+++ app.rb
@@ -6,3 +6,9 @@ set :haml, :format => :html5, :attr_wrapper => '"'
 get '/' do
   haml :index 
 end
+
+post '/count' do
+  text = params[:text]
+  words = text.delete("'").split /\W+/
+  "Count: #{words.count}"
+end
